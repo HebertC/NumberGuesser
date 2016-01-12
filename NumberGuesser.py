a5381e6202080de3d68262116a4684f097d30c43
qi8013so <hbrtxito@gmail.com>
2016-01-12 16:20:53 -0600

    Update the counter

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -3,7 +3,8 @@ __author__ = 'casey'
 import random
 
 def main():
-
+    #Guess Counter
+    counter = 0
     #varibles
     randNumber = random.randint(0,9)
     guessedNumber = 0
@@ -17,14 +18,16 @@ def main():
         if guessedNumber == randNumber:
             print(guessedNumber)
             print("That's correct! How did you know??")
+            print("Guess attempts  -> " + str(counter))
             break
 
         elif guessedNumber < randNumber:
             print(guessedNumber)
             print("Too low. Try again")
+            counter +=1
 
         elif guessedNumber > randNumber:
             print(guessedNumber)
             print("Too high. Try again")
-
+            counter +=1
 main()
